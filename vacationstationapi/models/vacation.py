351bcab0f25aaa7c118f1c402175304114c411f5
Ely Dorado <2018dorado@gmail.com>
2023-06-16 16:07:27 -0500

    post in progress

--- vacationstationapi/models/vacation.py
+++ vacationstationapi/models/vacation.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+ 
 
 class Vacation (models.Model):
 
     country = models.ForeignKey("Country", on_delete=models.CASCADE)
     city = models.CharField(max_length=80)
     vacation_type = models.ForeignKey("VacationType", on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    vacation_user = models.ForeignKey("VacationUser", on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=9000)
     number_of_people = models.IntegerField()
-    price = models.FloatField()
+    price = models.FloatField() 
     rating = models.IntegerField()
--- vacationstationapi/views/vacation.py
+++ vacationstationapi/views/vacation.py
@@ -3,8 +3,10 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from vacationstationapi.models import Vacation
-from vacationstationapi.models import VacationUser
+from vacationstationapi.models import Vacation, VacationType
+from vacationstationapi.models import Country, VacationUser
+from django.contrib.auth.models import User
+
 
 class VacationView(ViewSet):
     """Vacation Station Vacation View"""
@@ -27,6 +29,35 @@ class VacationView(ViewSet):
         serializer = VacationSerializer(vacation, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        vacation_user = VacationUser.objects.get(user=request.auth.user)
+        vacation_type = VacationType.objects.get(pk=request.data["vacation_type"])
+        country = Country.objects.get(pk=request.data["country"])
+# This is an outside model that gets used on line 47
+        vacation = Vacation.objects.create(
+            country=country,
+            city=request.data["city"],
+            vacation_type=vacation_type,
+            vacation_user=vacation_user,
+            description=request.data["description"],
+            number_of_people=request.data["number_of_people"],
+            price=request.data["price"],
+            rating=request.data["rating"]
+        )
+        serializer = VacationSerializer(vacation) #Front end needs this in JSON: Whole point of serializing
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+    
+
+    def destroy(self, request, pk):
+        vacation = Vacation.objects.get(pk=pk)
+        vacation.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
 class VacationSerializer(serializers.ModelSerializer):
     """
         JSON serializer for vacation 
@@ -34,4 +65,4 @@ class VacationSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Vacation
         fields = ('id', 'country', 'city', 'vacation_type',
-                    'user', 'description', 'number_of_people', 'price', 'rating')
+                    'vacation_user', 'description', 'number_of_people', 'price', 'rating')
