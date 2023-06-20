318cdb25f8afe3c206f53f2957bac2327b8a2183
Ely Dorado <2018dorado@gmail.com>
2023-06-20 14:43:48 -0500

    CR//UD

--- vacationstationapi/models/user_vacation.py
+++ vacationstationapi/models/user_vacation.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserVacation (models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    vacation_user = models.ForeignKey("VacationUser", on_delete=models.CASCADE, null = True)
     vacation = models.ForeignKey("Vacation", on_delete=models.CASCADE)
